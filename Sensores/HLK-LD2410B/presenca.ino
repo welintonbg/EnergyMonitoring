f3e526a860203386bbc25d8d55e6d135340040bf
welintonbg <68174758+welintonbg@users.noreply.github.com>
2024-06-22 17:34:48 -0300

    Update presenca.ino

--- Sensores/HLK-LD2410B/presenca.ino
+++ Sensores/HLK-LD2410B/presenca.ino
@@ -1,20 +1,3 @@
-/*
- * Example sketch for reporting on readings from the LD2410 using whatever settings are currently configured.
- * 
- * This has been tested on the following platforms...
- * 
- * On ESP32, connect the LD2410 to GPIO pins 32&33
- * On ESP32S2, connect the LD2410 to GPIO pins 8&9
- * On ESP32C3, connect the LD2410 to GPIO pins 4&5
- * On Arduino Leonardo or other ATmega32u4 board connect the LD2410 to GPIO pins TX & RX hardware serial
- * 
- * The serial configuration for other boards will vary and you'll need to assign them yourself
- * 
- * There is no example for ESP8266 as it only has one usable UART and will not boot if the alternate UART pins are used for the radar.
- * 
- * For this sketch and other examples to be useful the board needs to have two usable UARTs.
- * 
- */
 
 #define RADAR_SERIAL Serial1
 #define RADAR_RX_PIN 16
@@ -31,14 +14,10 @@ uint32_t presenca;
 
 void setup(void)
 {
-  Serial.begin(115200); //Feedback over Serial Monitor
+  Serial.begin(115200);
   RADAR_SERIAL.begin(256000, SERIAL_8N1, RADAR_RX_PIN, RADAR_TX_PIN); //UART for monitoring the radar
   delay(500);
-  Serial.print(F("\nConnect LD2410 radar TX to GPIO:"));
-  Serial.println(RADAR_RX_PIN);
-  Serial.print(F("Connect LD2410 radar RX to GPIO:"));
-  Serial.println(RADAR_TX_PIN);
-  Serial.print(F("LD2410 radar sensor initialising: "));
+  Serial.print(F("Iniciliazando: "));
   if(radar.begin(RADAR_SERIAL))
   {
     Serial.println(F("OK"));
@@ -60,7 +39,7 @@ void setup(void)
 uint32_t le_presenca()
 {
   radar.read();
-  if (radar.isConnected()) //Report every 1000ms
+  if (radar.isConnected())
   {
     if (radar.presenceDetected())
       presenca = 1;
@@ -82,13 +61,14 @@ uint32_t le_presenca()
       }
       Serial.println();
     }
-  }
-  else
-  {
-    presenca = 0;
-    Serial.println(F("No target"));
-  }
+  
+     else
+     {
+       presenca = 0;
+       Serial.println(F("No target"));
+     }
   return presenca;
+}
 }
 
 void loop(){
